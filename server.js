2e5d6b3df74318376477bb30611aa32ce93183dc
TylerG <tylergam@gmail.com>
2018-11-21 21:52:48 -0500

    another change

--- server.js
+++ server.js
@@ -19,10 +19,13 @@ app.use(cors(corsOptions))
 
 app.use(cors({
         //origin: config.origin,
-    credentials: false
+    origin: true,
+    credentials: true
 }));
 
 app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type');
     //res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
     if (req.method === "OPTIONS") {
         res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
