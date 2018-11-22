d120b7285b586aba6b9e0c228efac5715e7ec366
TylerG <tylergam@gmail.com>
2018-11-21 21:28:52 -0500

    minor change

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.use(cors({
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, Content-Type');
     //res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
     if (req.method === "OPTIONS") {
