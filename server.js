374237283fb8621c8055614358fc84b931164992
TylerG <tylergam@gmail.com>
2018-11-21 21:47:32 -0500

    try again

--- server.js
+++ server.js
@@ -19,10 +19,10 @@ app.use(cors(corsOptions))
 
 app.use(cors({
         //origin: config.origin,
+    credentials: false
 }));
 
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type');
     //res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
     if (req.method === "OPTIONS") {
         res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
